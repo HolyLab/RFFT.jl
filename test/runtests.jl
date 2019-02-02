53572e5ddc0a7316ffac124d66d236cfd47a07f5
Tim Holy <tim.holy@gmail.com>
2019-02-02 03:54:20 -0600

    Use a testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,23 +1,25 @@
 import RFFT
 using Test, FFTW, LinearAlgebra
 
-for region in (1:2, 1, 2)
-    for sz in ((5,6), (6,5))
-        pair = RFFT.RCpair{Float64}(undef, sz, region)
-        r = @inferred(real(pair))
-        c = @inferred(complex(pair))
-        b = rand(eltype(r), size(r))
-        copyto!(r, b)
-        RFFT.rfft!(pair)
-        RFFT.irfft!(pair)
-        @test r ≈ b
-        pfwd = RFFT.plan_rfft!(pair)
-        pinv = RFFT.plan_irfft!(pair)
-        pinv(pfwd(pair))
-        @test r ≈ b
+@testset begin
+    for region in (1:2, 1, 2)
+        for sz in ((5,6), (6,5))
+            pair = RFFT.RCpair{Float64}(undef, sz, region)
+            r = @inferred(real(pair))
+            c = @inferred(complex(pair))
+            b = rand(eltype(r), size(r))
+            copyto!(r, b)
+            RFFT.rfft!(pair)
+            RFFT.irfft!(pair)
+            @test r ≈ b
+            pfwd = RFFT.plan_rfft!(pair)
+            pinv = RFFT.plan_irfft!(pair)
+            pinv(pfwd(pair))
+            @test r ≈ b
 
-        pair2 = copy(pair)
-        @test real(pair2) == real(pair)
-        @test complex(pair2) == complex(pair)
+            pair2 = copy(pair)
+            @test real(pair2) == real(pair)
+            @test complex(pair2) == complex(pair)
+        end
     end
 end
